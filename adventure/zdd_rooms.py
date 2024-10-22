453142c388ee8a7aea1e35176ae872bcddc296a9
Florian Huber <florian.huber@hs-duesseldorf.de>
2024-10-22 17:58:27 +0200

    initial state of the game

---
+++ adventure/zdd_rooms.py
@@ -0,0 +1,28 @@
+"""This is to keep all special rooms of the ZDD."""
+from main_classes import Room
+
+
+class ToiletCellar(Room):
+    def run_story(self, user_items):
+        print("What did you expect? It's a toilet.")
+        if "old book" in [x.name for x in user_items]:
+            print("While you wash your hands, the book slips out of your backpack ...right into the water.")
+            print("You decide that it wasn't that important after all.")
+            # Remove book from inventory
+            return [x for x in user_items if x.name != "old book"]
+        return user_items
+
+## ----------------------------------------------------------------
+## List here all rooms
+
+toilet_cellar = ToiletCellar("toilet", "Yes, even the cellar has a toilet.")
+
+# Add your room instance here, similar to the example below:
+# my_room = MyRoom("room_name", "room_description")
+
+
+ALL_ROOMS = {
+    "toilet_cellar": toilet_cellar
+    # Add your room key-value pairs here:
+    # "my_room_key": my_room
+}
